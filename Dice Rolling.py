0ec56070692387ac828033ead1fa40418f35c317
Gowtham, Kumar <gowtham9394@gmail.com>
2020-07-19 15:17:31 +0530

    Second Commit

---
+++ Dice Rolling.py
@@ -0,0 +1,42 @@
+import random
+x = 'y'
+while x == 'y':
+     no = random.randint(1,6)
+     if no == 1:
+         print('***')
+         print('   ')
+         print(' 1 ')
+         print('   ')
+         print('***')
+     if no == 2:
+         print('***')
+         print('   ')
+         print(' 2 ')
+         print('   ')
+         print('***')
+     if no == 3:
+         print('***')
+         print('   ')
+         print(' 3 ')
+         print('   ')
+         print('***')
+     if no == 4:
+        print('***')
+        print('   ')
+        print(' 4 ')
+        print('   ')
+        print('***')
+     if no == 5:
+        print('***')
+        print('   ')
+        print(' 5 ')
+        print('   ')
+        print('***')
+     if no == 6:
+        print('***')
+        print('   ')
+        print(' 6 ')
+        print('   ')
+        print('***')
+     x = input("press y to roll the dice or press n to exit: ")
+print(X)
