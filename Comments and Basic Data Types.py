45f617e7acb976e9e33ac8dba68128fd2f60d15d
B <GowthamKumar.B@umlaut.com>
2020-12-02 19:04:37 +0530

    day1

---
+++ Comments and Basic Data Types.py
@@ -0,0 +1,51 @@
+# this is a comments
+print("Hello World")
+
+"""
+ This is a multi-Line comment
+
+"""
+
+"""
+Integers
+
+"""
+# the following are all integers
+
+print(0)
+print(12)
+print(345)
+print(6789)
+
+"""
+Floats
+
+"""
+# the following are all floats
+
+print(1.0) # even this number is a float
+print(2.1)
+print(3.45)
+print(6.789)
+
+"""
+Booleans
+
+"""
+
+# the following are booleans
+
+print(True)
+print(False)
+
+"""
+Strings
+
+"""
+
+# the following are strings
+
+print(" ")
+print("Python")
+print("Python is good for beginners.!")
+print('Ture')
