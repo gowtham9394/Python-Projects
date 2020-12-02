2cb6a51ff18bee824cf88f865e5e17b289d16730
B <GowthamKumar.B@umlaut.com>
2020-12-02 20:16:08 +0530

    day 2

---
+++ Variables.py
@@ -0,0 +1,45 @@
+# the following are all Variables.
+
+first_name = "Gowtham"
+print(first_name)        # this print output as 'Gowtham"
+
+# Handling Naming Errors
+
+Sports = "Cricket" # capital 'S'
+print(Sports)      # lowercase 's'
+
+# If we try to run this code, we’ll get the following error/output: NameError: name 'sport' is not defined
+
+# storing Integer and Float Variables
+
+num1 = 5                           # storing an integer into a variable
+num2 = 5.6                         # storing a float into a variable
+print( num1, num2)                 # you can print multiple items using commas
+
+# storing a boolean into a variable
+
+cup = True
+print(cup)
+
+# storing strings into a variable
+
+name = "Gowtham"
+number =  '9394'
+print(name, number)                 # this print 9394 next to the name
+
+# using two variables to create another variable
+
+output = num1 + num2
+print(output)                       # this will print additon value of num1 value (5) from line 15 and num2 value (16) from line 16.
+
+# adding, deleting, multiplying, dividing from a variable
+
+output += 1
+print(output)                       # same as saying output = output + 1
+output *= num1
+print(output)                       # same as saying output = output * num1
+
+# defining a variable and overwriting it's value
+
+name = "Kumar"
+print(name)    
