4df2b3d34f67a166eee433f67b790bb181bba4ea
B <GowthamKumar.B@umlaut.com>
2020-12-04 11:03:28 +0530

    Day-4

---
+++ String Manipulation.py
@@ -0,0 +1,22 @@
+# using the title method to capitalize a string
+
+name = "gowtham Kumar"
+print(name.title())
+
+# replacing an exclamation point with a period
+
+word = "hello world!"
+print(word.replace("!","."))
+
+# finding the starting index of our searched term
+
+s = "hello world, what is happeing now"
+print(s.find("now"))
+
+# removing white space with strip
+
+name = "   Gowtham   "
+print( name.strip( ))
+
+# converting a string into a list of words
+print(s.split(" "))
